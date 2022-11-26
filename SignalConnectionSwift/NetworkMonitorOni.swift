5e69b0950b1d7e4a446742ff6db2c5274b4f808b
etho <>
2022-11-25 16:32:56 -0800

    working on monitoring network class still

--- SignalConnectionSwift/NetworkMonitorOni.swift
+++ SignalConnectionSwift/NetworkMonitorOni.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Network
+//singleton class to wrap calls into network framework
+final class NetWorkMonitor {
+    static let shared = NetWorkMonitor()
+    private let queue = DispatchQueue.global()
+    private let monitor : NWPathMonitor
+    public private(set) var isConnected : Bool = false
+    
+    private init(){
+        monitor = NWPathMonitor()
+    }
+    
+    public func StartMonitoring(){
+        monitor.start(queue: queue)
+        monitor.pathUpdateHandler = {[weak self] path in
+            self?.isConnected = path.status == .satisfied
+        }
+    }
+    
+    public func StopMonitoring(){
+        monitor.cancel()
+    }
+}
